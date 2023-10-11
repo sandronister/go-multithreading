af7dad37c4843c1d96537d4cd4ee850e4ea90df8
Julio Sandroni <sandronister@gmail.com>
2023-10-11 09:14:44 -0300

    first commit

---
+++ 1-Goroutines/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	go task("A")
+	go task("B")
+	time.Sleep(15 * time.Second)
+}
