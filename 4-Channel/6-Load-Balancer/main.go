3e6c93851ddf684824916513afd23649cb6bca1e
Julio Sandroni <sandronister@gmail.com>
2023-10-12 18:54:46 -0300

    load balancer

---
+++ 4-Channel/6-Load-Balancer/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(workerID int, data <-chan int) {
+	for x := range data {
+		fmt.Printf("Worker %d received %d\n", workerID, x)
+		time.Sleep(20 * time.Millisecond)
+	}
+}
+
+func main() {
+	data := make(chan int)
+	qtdWorks := 1000
+
+	for i := 0; i <= qtdWorks; i++ {
+		go worker(i, data)
+	}
+
+	for i := 0; i <= 10000; i++ {
+		data <- i
+	}
+}
